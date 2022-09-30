8699260eac4e91a787d7066074c3081e251a2c1d
ehilariosha <113388075+ehilariosha@users.noreply.github.com>
2022-09-30 14:23:35 -0700

    working on business

--- main.ts
+++ main.ts
@@ -6,11 +6,42 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
     fireball.setFlag(SpriteFlag.Invisible, false)
+    rocketflag = 1
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
+    if (Math.abs(apple.vx) > 20 || apple.vy > 50) {
+        boom = sprites.create(img`
+            . 3 . . . . . . . . . . . 4 . . 
+            . 3 3 . . . . . . . . . 4 4 . . 
+            . 3 d 3 . . 4 4 . . 4 4 d 4 . . 
+            . . 3 5 3 4 5 5 4 4 d d 4 4 . . 
+            . . 3 d 5 d 1 1 d 5 5 d 4 4 . . 
+            . . 4 5 5 1 1 1 1 5 1 1 5 4 . . 
+            . 4 5 5 5 5 1 1 5 1 1 1 d 4 4 . 
+            . 4 d 5 1 1 5 5 5 1 1 1 5 5 4 . 
+            . 4 4 5 1 1 5 5 5 5 5 d 5 5 4 . 
+            . . 4 3 d 5 5 5 d 5 5 d d d 4 . 
+            . 4 5 5 d 5 5 5 d d d 5 5 4 . . 
+            . 4 5 5 d 3 5 d d 3 d 5 5 4 . . 
+            . 4 4 d d 4 d d d 4 3 d d 4 . . 
+            . . 4 5 4 4 4 4 4 4 4 4 4 . . . 
+            . 4 5 4 . . 4 4 4 . . . 4 4 . . 
+            . 4 4 . . . . . . . . . . 4 4 . 
+            `, SpriteKind.Projectile)
+        boom.setPosition(apple.x, apple.y)
+        scene.cameraFollowSprite(boom)
+        music.bigCrash.playUntilDone()
+        pause(1000)
+        game.over(false)
+    } else {
+        if (landingflag == 0) {
+            fuel += 100
+            landingflag = 1
+        }
+    }
     apple.setVelocity(0, -1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -19,32 +50,40 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
     fireball.setFlag(SpriteFlag.Invisible, true)
+    rocketflag = 0
 })
+let boom: Sprite = null
 let apple: Sprite = null
 let angle = 0
 let fireball: Sprite = null
+let landingflag = 0
+let rocketflag = 0
+let fuel = 1000
+let fuelSprite = textsprite.create(convertToText(fuel))
+rocketflag = 0
+landingflag = 0
 fireball = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 4 4 4 4 . . . . . . 
-    . . . . 4 4 4 5 5 4 4 4 . . . . 
-    . . . 3 3 3 3 4 4 4 4 4 4 . . . 
-    . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
-    . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
-    . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
-    . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
-    . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
-    . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
-    . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
-    . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
-    . . . 4 2 2 2 2 2 2 2 2 4 . . . 
-    . . . . 4 4 2 2 2 2 4 4 . . . . 
-    . . . . . . 4 4 4 4 . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 3 . . . . . . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . 3 3 . . 3 1 3 . . 3 3 . . . 
+    . . 3 1 3 . 3 1 3 2 3 1 3 . . . 
+    . . . 3 1 3 3 1 3 2 1 3 . . . . 
+    3 3 3 3 2 1 3 1 1 1 3 . . . . . 
+    3 1 1 1 1 1 1 1 1 2 3 3 3 3 3 3 
+    . 3 3 3 2 3 1 1 1 1 1 1 1 1 1 3 
+    . . . . . 2 1 1 1 3 3 2 3 3 3 . 
+    . . . . 3 1 3 1 3 1 2 . . . . . 
+    . . . 3 1 3 2 1 3 3 1 3 . . . . 
+    . . 3 1 3 . 2 1 3 . 3 1 3 . . . 
+    . . 3 3 . . 3 1 3 . . 3 3 . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . . . . . 3 3 . . . . . . . . 
     `, SpriteKind.Projectile)
 fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
 tiles.setCurrentTilemap(tilemap`level2`)
-effects.clouds.startScreenEffect()
+effects.starField.startScreenEffect()
 apple = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
     . . . . e e e c 7 7 e e . . . . 
@@ -73,6 +112,9 @@ apple.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
+    fuelSprite.destroy()
+    fuelSprite = textsprite.create(convertToText(fuel))
+    fuelSprite.setPosition(apple.x, apple.top)
     minimap2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
     minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
@@ -80,4 +122,10 @@ game.onUpdate(function () {
     minimap2.setPosition(apple.x - 50, apple.y - 30)
     engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
     fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    if (rocketflag == 1) {
+        fuel += -1
+    }
+    if (apple.y < 150) {
+        landingflag = 0
+    }
 })
